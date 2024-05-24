0b787fa1edce5208dab757d08f0e7dc8b579395d
Joaquin Bejar <jb@taunais.com>
2024-05-24 18:14:42 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
+
 # StrategyExecutionEngine
-A Rust library for creating and executing market order strategies with Kafka integration.
+
+StrategyExecutionEngine is a Rust library for creating and executing market order strategies with integration for various messaging systems including Kafka, NATS, RabbitMQ, ZeroMQ, and Redis.
+
+## Features
+
+- **High Performance**: Optimized for handling high volumes of orders in real-time with low latency.
+- **Real-Time Data Processing**: Ensures timely and efficient processing of parent and child orders.
+- **Easy Integration**: Easily integrates with multiple messaging systems such as Kafka, NATS, RabbitMQ, ZeroMQ, and Redis for seamless order handling.
+- **Flexible Strategies**: Supports various order execution strategies.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+What things you need to install the software and how to install them:
+
+```bash
+# Example: Installation of Rust and Cargo (Rust's package manager)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Installation of Kafka (using Homebrew as an example)
+brew install kafka
+```
+
+### Installing
+
+A step-by-step series of examples that tell you how to get a development environment running:
+
+```bash
+# Clone the repository
+git clone https://github.com/joaquinbejar/StrategyExecutionEngine.git
+
+# Go into the repository
+cd StrategyExecutionEngine
+
+# Build the project
+cargo build
+```
+
+### Running the tests
+
+```bash
+cargo test
+```
+
+### Deployment
+
+Add additional notes about how to deploy this on a live system.
+
+### Contributing
+
+Please read CONTRIBUTING.md for details on our code of conduct, and the process for submitting pull requests to us.
+
+### Versioning
+
+We use [SemVer](http://semver.org/) for versioning. This approach allows us to maintain a clear, predictable system for version management. Under this scheme, version numbers are given in the format of \`MAJOR.MINOR.PATCH\`, where:
+
+- `MAJOR` versions indicate incompatible API changes,
+- `MINOR` versions add functionality in a backwards-compatible manner, and
+- `PATCH` versions include backwards-compatible bug fixes.
+
+This standard helps users and developers to understand the impact of new updates at a glance. For the versions available, see the [tags on this repository](https://github.com/joaquinbejar/StrategyExecutionEngine/tags).
+
+### Authors
+
+Joaquín Béjar García - Initial work - [joaquinbejar](https://github.com/joaquinbejar)
+
+See also the list of contributors who participated in this project.
+
+### License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details.
+
+### Acknowledgments
+
+Will be added in the future.
---
+++ src/config.rs
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ Copyright (c) 2024.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ******************************************************************************/
+
+/******************************************************************************
+    Author:
+    Email: jb@taunais.com
+    Date: 24/5/24
+ ******************************************************************************/
+
+use serde_json::json;
+use std::env;
+use thiserror::Error;
+use crate::constants::{*};
+
+/// Represents the configuration options for the application.
+#[derive(Debug, Clone)]
+pub struct Config {
+    /// The WebSocket URL for connecting to the server.
+    pub kafka_url: String,
+}
+
+/// An enum representing various errors that can occur during configuration.
+#[derive(Error, Debug)]
+pub enum ConfigError {
+    /// Error indicating that a required environment variable is missing.
+    #[error("missing environment variable: {0}")]
+    MissingEnvVar(String),
+}
+
+impl Config {
+    /// Creates a new `Config` instance by reading environment variables.
+    ///
+    /// # Errors
+    ///
+    /// Returns a `ConfigError` if a required environment variable is missing.
+    pub fn new() -> Result<Self, ConfigError> {
+        Ok(Config {
+            kafka_url: Self::get_env_var_or_default("KAFKA_URL", KAFKA_URL.to_string())
+        })
+    }
+
+    /// Gets the value of an environment variable or returns a default value if the variable is not set.
+    ///
+    /// # Arguments
+    ///
+    /// * `var_name` - The name of the environment variable.
+    /// * `default_value` - The default value to use if the environment variable is not set.
+    fn get_env_var_or_default(var_name: &str, default_value: String) -> String {
+        env::var(var_name).unwrap_or(default_value)
+    }
+
+    /// Gets the value of an environment variable or returns an error if the variable is not set.
+    ///
+    /// # Arguments
+    ///
+    /// * `var_name` - The name of the environment variable.
+    ///
+    /// # Errors
+    ///
+    /// Returns a `ConfigError::MissingEnvVar` if the environment variable is not set.
+    fn get_env_var_or_error(var_name: &str) -> Result<String, ConfigError> {
+        env::var(var_name).map_err(|_| ConfigError::MissingEnvVar(var_name.to_string()))
+    }
+
+    /// Serializes the configuration to a JSON string.
+    ///
+    /// # Returns
+    ///
+    /// Returns a JSON string representation of the configuration.
+    ///
+    /// # Errors
+    ///
+    /// Returns a `serde_json::Error` if serialization fails.
+    pub fn print_as_json(&self) -> serde_json::Result<String> {
+        let json_config = json!({
+            "KAFKA_URL": self.kafka_url,
+        });
+        serde_json::to_string_pretty(&json_config)
+    }
+}
