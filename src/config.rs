3fec51448c5dccf51dab8e39a5fa9fd18c31f9d0
Joaquin Bejar <jb@taunais.com>
2024-05-24 19:47:02 +0200

    define Config and sub-structs

--- src/config.rs
+++ src/config.rs
@@ -26,17 +26,87 @@
     Date: 24/5/24
  ******************************************************************************/
 
+use serde::Serialize;
 use serde_json::json;
+use serde_json::Result as JsonResult;
 use std::env;
 use thiserror::Error;
-use crate::constants::{*};
 
-/// Represents the configuration options for the application.
-#[derive(Debug, Clone)]
-pub struct Config {
-    /// The WebSocket URL for connecting to the server.
+/// Trait for serializing structures to JSON.
+pub trait JsonSerializable {
+    fn print_as_json(&self) -> JsonResult<String>;
+}
+
+/// Represents the configuration options for Kafka.
+#[derive(Debug, Clone, Serialize)]
+pub struct KafkaConfig {
     pub kafka_url: String,
+}
+
+/// Represents the configuration options for NATS.
+#[derive(Debug, Clone, Serialize)]
+pub struct NatsConfig {
     pub nats_url: String,
+}
+
+/// Represents the configuration options for RabbitMQ.
+#[derive(Debug, Clone, Serialize)]
+pub struct RabbitMqConfig {
+    pub rabbitmq_url: String,
+}
+
+/// Represents the configuration options for ZeroMQ.
+#[derive(Debug, Clone, Serialize)]
+pub struct ZeroMqConfig {
+    pub zmq_url: String,
+}
+
+/// Represents the configuration options for Redis.
+#[derive(Debug, Clone, Serialize)]
+pub struct RedisConfig {
+    pub redis_url: String,
+}
+
+// Implementing JsonSerializable trait for each messaging configuration structure.
+
+impl JsonSerializable for KafkaConfig {
+     fn print_as_json(&self) -> JsonResult<String> {
+        serde_json::to_string_pretty(self)
+    }
+}
+
+impl JsonSerializable for NatsConfig {
+    fn print_as_json(&self) -> JsonResult<String> {
+        serde_json::to_string_pretty(self)
+    }
+}
+
+impl JsonSerializable for RabbitMqConfig {
+    fn print_as_json(&self) -> JsonResult<String> {
+        serde_json::to_string_pretty(self)
+    }
+}
+
+impl JsonSerializable for ZeroMqConfig {
+    fn print_as_json(&self) -> JsonResult<String> {
+        serde_json::to_string_pretty(self)
+    }
+}
+
+impl JsonSerializable for RedisConfig {
+    fn print_as_json(&self) -> JsonResult<String> {
+        serde_json::to_string_pretty(self)
+    }
+}
+
+/// Represents the general configuration options for the application.
+#[derive(Debug, Clone, Serialize)]
+pub struct Config {
+    pub kafka: Option<KafkaConfig>,
+    pub nats: Option<NatsConfig>,
+    pub rabbitmq: Option<RabbitMqConfig>,
+    pub zeromq: Option<ZeroMqConfig>,
+    pub redis: Option<RedisConfig>,
 }
 
 /// An enum representing various errors that can occur during configuration.
@@ -55,47 +125,52 @@ impl Config {
     /// Returns a `ConfigError` if a required environment variable is missing.
     pub fn new() -> Result<Self, ConfigError> {
         Ok(Config {
-            kafka_url: Self::get_env_var_or_default("KAFKA_URL", KAFKA_URL.to_string()),
-            nats_url: Self::get_env_var_or_error("NATS_URL")?,
+            kafka: Self::get_kafka_config(),
+            nats: Self::get_nats_config()?,
+            rabbitmq: Self::get_rabbitmq_config(),
+            zeromq: Self::get_zeromq_config(),
+            redis: Self::get_redis_config(),
         })
     }
 
-    /// Gets the value of an environment variable or returns a default value if the variable is not set.
-    ///
-    /// # Arguments
-    ///
-    /// * `var_name` - The name of the environment variable.
-    /// * `default_value` - The default value to use if the environment variable is not set.
-    fn get_env_var_or_default(var_name: &str, default_value: String) -> String {
-        env::var(var_name).unwrap_or(default_value)
+    /// Gets the Kafka configuration from environment variables.
+    fn get_kafka_config() -> Option<KafkaConfig> {
+        env::var("KAFKA_URL").ok().map(|kafka_url| KafkaConfig { kafka_url })
     }
 
-    /// Gets the value of an environment variable or returns an error if the variable is not set.
-    ///
-    /// # Arguments
-    ///
-    /// * `var_name` - The name of the environment variable.
-    ///
-    /// # Errors
-    ///
-    /// Returns a `ConfigError::MissingEnvVar` if the environment variable is not set.
-    fn get_env_var_or_error(var_name: &str) -> Result<String, ConfigError> {
-        env::var(var_name).map_err(|_| ConfigError::MissingEnvVar(var_name.to_string()))
+    /// Gets the NATS configuration from environment variables.
+    fn get_nats_config() -> Result<Option<NatsConfig>, ConfigError> {
+        match env::var("NATS_URL") {
+            Ok(nats_url) => Ok(Some(NatsConfig { nats_url })),
+            Err(_) => Ok(None),
+        }
     }
 
-    /// Serializes the configuration to a JSON string.
-    ///
-    /// # Returns
-    ///
-    /// Returns a JSON string representation of the configuration.
-    ///
-    /// # Errors
-    ///
-    /// Returns a `serde_json::Error` if serialization fails.
-    pub fn print_as_json(&self) -> serde_json::Result<String> {
+    /// Gets the RabbitMQ configuration from environment variables.
+    fn get_rabbitmq_config() -> Option<RabbitMqConfig> {
+        env::var("RABBITMQ_URL").ok().map(|rabbitmq_url| RabbitMqConfig { rabbitmq_url })
+    }
+
+    /// Gets the ZeroMQ configuration from environment variables.
+    fn get_zeromq_config() -> Option<ZeroMqConfig> {
+        env::var("ZMQ_URL").ok().map(|zmq_url| ZeroMqConfig { zmq_url })
+    }
+
+    /// Gets the Redis configuration from environment variables.
+    fn get_redis_config() -> Option<RedisConfig> {
+        env::var("REDIS_URL").ok().map(|redis_url| RedisConfig { redis_url })
+    }
+}
+
+// Implementing JsonSerializable trait for Config structure.
+impl JsonSerializable for Config {
+    fn print_as_json(&self) -> JsonResult<String> {
         let json_config = json!({
-            "KAFKA_URL": self.kafka_url,
-            "NATS_URL": self.nats_url,
+            "kafka": self.kafka.as_ref().map(|k| k.print_as_json().unwrap_or_else(|_| "Error serializing KafkaConfig".to_string())),
+            "nats": self.nats.as_ref().map(|n| n.print_as_json().unwrap_or_else(|_| "Error serializing NatsConfig".to_string())),
+            "rabbitmq": self.rabbitmq.as_ref().map(|r| r.print_as_json().unwrap_or_else(|_| "Error serializing RabbitMqConfig".to_string())),
+            "zeromq": self.zeromq.as_ref().map(|z| z.print_as_json().unwrap_or_else(|_| "Error serializing ZeroMqConfig".to_string())),
+            "redis": self.redis.as_ref().map(|r| r.print_as_json().unwrap_or_else(|_| "Error serializing RedisConfig".to_string())),
         });
         serde_json::to_string_pretty(&json_config)
     }
--- tests/unit/config_test.rs
+++ tests/unit/config_test.rs
@@ -1,27 +1,139 @@
 /*******************************************************************************
- Copyright (c) 2024.
+Copyright (c) 2024.
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ******************************************************************************/
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+******************************************************************************/
 
 /******************************************************************************
-    Author:  
-    Email: jb@taunais.com 
-    Date: 24/5/24
- ******************************************************************************/
+   Author:
+   Email: jb@taunais.com
+   Date: 24/5/24
+******************************************************************************/
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use strategy_execution_engine::config::{
+        Config, JsonSerializable, KafkaConfig, NatsConfig, RabbitMqConfig, RedisConfig,
+        ZeroMqConfig,
+    };
+    use std::sync::Mutex;
+    use lazy_static::lazy_static;
+
+    lazy_static! {
+        static ref ENV_MUTEX: Mutex<()> = Mutex::new(());
+    }
+
+
+    #[test]
+    fn test_kafka_config_serialization() {
+        let kafka_config = KafkaConfig {
+            kafka_url: "localhost:9092".to_string(),
+        };
+        let json = kafka_config.print_as_json().unwrap();
+        assert_eq!(json, "{\n  \"kafka_url\": \"localhost:9092\"\n}");
+    }
+
+    #[test]
+    fn test_nats_config_serialization() {
+        let nats_config = NatsConfig {
+            nats_url: "nats://localhost:4222".to_string(),
+        };
+        let json = nats_config.print_as_json().unwrap();
+        assert_eq!(json, "{\n  \"nats_url\": \"nats://localhost:4222\"\n}");
+    }
+
+    #[test]
+    fn test_rabbitmq_config_serialization() {
+        let rabbitmq_config = RabbitMqConfig {
+            rabbitmq_url: "amqp://localhost:5672".to_string(),
+        };
+        let json = rabbitmq_config.print_as_json().unwrap();
+        assert_eq!(json, "{\n  \"rabbitmq_url\": \"amqp://localhost:5672\"\n}");
+    }
+
+    #[test]
+    fn test_zeromq_config_serialization() {
+        let zeromq_config = ZeroMqConfig {
+            zmq_url: "tcp://localhost:5555".to_string(),
+        };
+        let json = zeromq_config.print_as_json().unwrap();
+        assert_eq!(json, "{\n  \"zmq_url\": \"tcp://localhost:5555\"\n}");
+    }
+
+    #[test]
+    fn test_redis_config_serialization() {
+        let redis_config = RedisConfig {
+            redis_url: "redis://localhost:6379".to_string(),
+        };
+        let json = redis_config.print_as_json().unwrap();
+        assert_eq!(json, "{\n  \"redis_url\": \"redis://localhost:6379\"\n}");
+    }
+
+    #[test]
+    fn test_config_initialization_and_serialization() {
+        let _guard = ENV_MUTEX.lock().unwrap();
+        // Setting environment variables for the test
+        env::set_var("KAFKA_URL", "localhost:9092");
+        env::set_var("NATS_URL", "nats://localhost:4222");
+        env::set_var("RABBITMQ_URL", "amqp://localhost:5672");
+        env::set_var("ZMQ_URL", "tcp://localhost:5555");
+        env::set_var("REDIS_URL", "redis://localhost:6379");
+
+        let config = Config::new().unwrap();
+        let json = config.print_as_json().unwrap();
+
+        let expected_json = r#"
+{
+  "kafka": "{\n  \"kafka_url\": \"localhost:9092\"\n}",
+  "nats": "{\n  \"nats_url\": \"nats://localhost:4222\"\n}",
+  "rabbitmq": "{\n  \"rabbitmq_url\": \"amqp://localhost:5672\"\n}",
+  "redis": "{\n  \"redis_url\": \"redis://localhost:6379\"\n}",
+  "zeromq": "{\n  \"zmq_url\": \"tcp://localhost:5555\"\n}"
+}"#;
+
+        println!("{}", json);
+        assert_eq!(json, expected_json.trim());
+    }
+
+    #[test]
+    fn test_missing_env_vars() {
+        let _guard = ENV_MUTEX.lock().unwrap();
+        // Unset all environment variables
+        env::remove_var("KAFKA_URL");
+        env::remove_var("NATS_URL");
+        env::remove_var("RABBITMQ_URL");
+        env::remove_var("ZMQ_URL");
+        env::remove_var("REDIS_URL");
+
+        let config = Config::new().unwrap();
+        let json = config.print_as_json().unwrap();
+
+        let expected_json = r#"
+{
+  "kafka": null,
+  "nats": null,
+  "rabbitmq": null,
+  "redis": null,
+  "zeromq": null
+}"#;
+
+        assert_eq!(json, expected_json.trim());
+    }
+}
