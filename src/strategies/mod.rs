ba3308e416db756869182276bd1d3ca04b0eba80
Joaquin Bejar <jb@taunais.com>
2024-05-25 09:38:10 +0200

    Strategies folder structure

---
+++ src/strategies/mod.rs
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ Copyright (c) 2024.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ******************************************************************************/
+
+/******************************************************************************
+    Author: Joaquín Béjar García
+    Email: jb@taunais.com 
+    Date: 25/5/24
+ ******************************************************************************/
+pub mod inventory_based;
+pub mod time_volume_based;
+pub mod algo_based;
+pub mod market_microstructure_based;
+pub mod dark_pool_based;
+pub mod technical_indicator_based;
+pub mod common;
+mod inventory_based;
+mod algo_based;
+mod dark_pool_based;
+mod market_microstructure_based;
+mod technical_indicator_based;
+mod time_volume_based;
+
+pub use inventory_based::*;
+pub use time_volume_based::*;
+pub use algo_based::*;
+pub use market_microstructure_based::*;
+pub use dark_pool_based::*;
+pub use technical_indicator_based::*;
+pub use common::*;
